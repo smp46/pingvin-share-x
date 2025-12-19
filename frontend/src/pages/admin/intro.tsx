584eeb766a1be9d5ddc6b93261a5b425f5bfa222
smp46 <me@smp46.me>
2025-12-19 11:35:22 +1000

    fix: references to old project name

--- frontend/src/pages/admin/intro.tsx
+++ frontend/src/pages/admin/intro.tsx
@@ -21,10 +21,10 @@ const Intro = () => {
             <Logo height={80} width={80} />
           </Center>
           <Center>
-            <Title order={2}>Welcome to Pingvin Share</Title>
+            <Title order={2}>Welcome to Pingvin Share X</Title>
           </Center>
           <Text>
-            If you enjoy Pingvin Share please ⭐️ it on{" "}
+            If you enjoy Pingvin Share X please ⭐️ it on{" "}
             <Anchor
               target="_blank"
               href="https://github.com/smp46/pingvin-share-x"
@@ -32,14 +32,14 @@ const Intro = () => {
               GitHub
             </Anchor>{" "}
           </Text>
-          <Text>Enough talked, have fun with Pingvin Share-X!</Text>
+          <Text>Enough talked, have fun with Pingvin Share X!</Text>
           <Text mt="lg">How to you want to continue?</Text>
           <Stack>
             <Button href="/admin/config/general" component={Link}>
               Customize configuration
             </Button>
             <Button href="/" component={Link} variant="light">
-              Explore Pingvin Share
+              Explore Pingvin Share X
             </Button>
           </Stack>
         </Stack>
