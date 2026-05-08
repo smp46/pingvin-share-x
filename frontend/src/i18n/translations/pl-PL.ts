cc4464bb85e11845ca5cfeffb5a5e6be2f77ec6c
smp46 <me@smp46.me>
2026-05-08 09:16:45 +0100

    New translations en-us.ts (Polish)
    [ci skip]

--- frontend/src/i18n/translations/pl-PL.ts
+++ frontend/src/i18n/translations/pl-PL.ts
@@ -217,6 +217,7 @@ export default {
 
   // /admin/shares
   "admin.shares.title": "Zarządzanie udostępnieniami",
+  "admin.shares.diskUsage": "Disk Usage",
   "admin.shares.table.id": "ID Udostępnienia",
   "admin.shares.table.username": "Twórca",
   "admin.shares.table.visitors": "Odwiedzający",
