769cb5718c78d884c7138d1fb15363e7c2076a72
smp46 <me@smp46.me>
2026-05-08 09:17:09 +0100

    New translations en-us.ts (Arabic, Egypt)
    [ci skip]

--- frontend/src/i18n/translations/ar-EG.ts
+++ frontend/src/i18n/translations/ar-EG.ts
@@ -217,6 +217,7 @@ export default {
 
   // /admin/shares
   "admin.shares.title": "إدارة المشاركة",
+  "admin.shares.diskUsage": "Disk Usage",
   "admin.shares.table.id": "معرّف المشاركة",
   "admin.shares.table.username": "المُنشئ",
   "admin.shares.table.visitors": "الزوار",
