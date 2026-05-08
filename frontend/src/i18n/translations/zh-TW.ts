b73a35876c8e8aca2c1caa59f68c0baee0fcd65a
smp46 <me@smp46.me>
2026-05-08 09:16:57 +0100

    New translations en-us.ts (Chinese Traditional)
    [ci skip]

--- frontend/src/i18n/translations/zh-TW.ts
+++ frontend/src/i18n/translations/zh-TW.ts
@@ -217,6 +217,7 @@ export default {
 
   // /admin/shares
   "admin.shares.title": "檔案管理",
+  "admin.shares.diskUsage": "Disk Usage",
   "admin.shares.table.id": "分享 ID",
   "admin.shares.table.username": "建立者",
   "admin.shares.table.visitors": "造訪次數",
