b554ea73439e762f4d2e8bdaf6d7dfd7b8565b82
smp46 <me@smp46.me>
2026-05-14 12:52:10 +0100

    New translations en-us.ts (Chinese Traditional)
    [ci skip]

--- frontend/src/i18n/translations/zh-TW.ts
+++ frontend/src/i18n/translations/zh-TW.ts
@@ -383,7 +383,7 @@ export default {
   "admin.config.cache.test-redis.modal.save.description": "To continue you need to save the configuration first. Do you want to save the configuration and test the Redis connection?",
   "admin.config.cache.test-redis.modal.save.confirm": "Save and test",
   "admin.config.email.enable-share-email-recipients": "啟用電子郵件收件者分享",
-  "admin.config.email.enable-share-email-recipients.description": "是否允許與收件者分享電子郵件。僅當 SMTP 已啟用時才啟用此功能。",
+  "admin.config.email.enable-share-email-recipients.description": "Whether to allow email sharing with recipients. This can only be enabled if SMTP is activated.",
   "admin.config.email.share-recipients-subject": "分享Email通知主題",
   "admin.config.email.share-recipients-subject.description": "發送分享Email通知的主題",
   "admin.config.email.share-recipients-message": "分享Email通知內容",
