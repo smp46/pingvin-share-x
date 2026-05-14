55b090b01e597ce25e670684050a7d3a3eb2e714
smp46 <me@smp46.me>
2026-05-14 12:52:17 +0100

    New translations en-us.ts (Estonian)
    [ci skip]

--- frontend/src/i18n/translations/et-EE.ts
+++ frontend/src/i18n/translations/et-EE.ts
@@ -383,7 +383,7 @@ export default {
   "admin.config.cache.test-redis.modal.save.description": "To continue you need to save the configuration first. Do you want to save the configuration and test the Redis connection?",
   "admin.config.cache.test-redis.modal.save.confirm": "Save and test",
   "admin.config.email.enable-share-email-recipients": "Luba e-posti teavitused jagamise saajatele",
-  "admin.config.email.enable-share-email-recipients.description": "Kas lubada e-posti teavitusi jagamise saajatele. Lubage ainult, kui SMTP on aktiveeritud.",
+  "admin.config.email.enable-share-email-recipients.description": "Whether to allow email sharing with recipients. This can only be enabled if SMTP is activated.",
   "admin.config.email.share-recipients-subject": "Jagamise saajate pealkiri",
   "admin.config.email.share-recipients-subject.description": "Pealkiri mis saadetakse jagamise saajatele.",
   "admin.config.email.share-recipients-message": "Jagamise saajate sõnum",
