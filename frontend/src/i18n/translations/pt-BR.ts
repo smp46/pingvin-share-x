41e432481ad22dea99de922bc1f8c4a7c13ac53d
smp46 <me@smp46.me>
2026-05-14 12:52:13 +0100

    New translations en-us.ts (Portuguese, Brazilian)
    [ci skip]

--- frontend/src/i18n/translations/pt-BR.ts
+++ frontend/src/i18n/translations/pt-BR.ts
@@ -383,7 +383,7 @@ export default {
   "admin.config.cache.test-redis.modal.save.description": "To continue you need to save the configuration first. Do you want to save the configuration and test the Redis connection?",
   "admin.config.cache.test-redis.modal.save.confirm": "Save and test",
   "admin.config.email.enable-share-email-recipients": "Ativar compartilhamento por email",
-  "admin.config.email.enable-share-email-recipients.description": "Se deseja permitir o compartilhamento por e-mail com destinatários. Só ative isto se o SMTP estiver ativado.",
+  "admin.config.email.enable-share-email-recipients.description": "Whether to allow email sharing with recipients. This can only be enabled if SMTP is activated.",
   "admin.config.email.share-recipients-subject": "Assunto dos destinatários do compartilhamento",
   "admin.config.email.share-recipients-subject.description": "Assunto do e-mail enviado para os destinatários do compartilhamento.",
   "admin.config.email.share-recipients-message": "Assunto dos destinatários do compartilhamento",
