b7d7410c859c44cdacf1f15b2f7d04de214cc2b3
smp46 <me@smp46.me>
2026-05-14 12:51:53 +0100

    New translations en-us.ts (Finnish)
    [ci skip]

--- frontend/src/i18n/translations/fi-FI.ts
+++ frontend/src/i18n/translations/fi-FI.ts
@@ -383,7 +383,7 @@ export default {
   "admin.config.cache.test-redis.modal.save.description": "To continue you need to save the configuration first. Do you want to save the configuration and test the Redis connection?",
   "admin.config.cache.test-redis.modal.save.confirm": "Save and test",
   "admin.config.email.enable-share-email-recipients": "Enable email recipient sharing",
-  "admin.config.email.enable-share-email-recipients.description": "Whether to allow email sharing with recipients. Only enable this if SMTP is activated.",
+  "admin.config.email.enable-share-email-recipients.description": "Whether to allow email sharing with recipients. This can only be enabled if SMTP is activated.",
   "admin.config.email.share-recipients-subject": "Sähköpostijaon otsikko",
   "admin.config.email.share-recipients-subject.description": "Sähköpostin aihe, joka saa lähetetään jaon vastaanottajille.",
   "admin.config.email.share-recipients-message": "Sähköpostijaon viesti",
