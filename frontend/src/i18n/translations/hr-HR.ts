10671bdfcd5e40f0cb0fd1969e3ec5645ae5e4e7
smp46 <me@smp46.me>
2026-05-14 12:52:16 +0100

    New translations en-us.ts (Croatian)
    [ci skip]

--- frontend/src/i18n/translations/hr-HR.ts
+++ frontend/src/i18n/translations/hr-HR.ts
@@ -383,7 +383,7 @@ export default {
   "admin.config.cache.test-redis.modal.save.description": "To continue you need to save the configuration first. Do you want to save the configuration and test the Redis connection?",
   "admin.config.cache.test-redis.modal.save.confirm": "Save and test",
   "admin.config.email.enable-share-email-recipients": "Omogućite dijeljenje putem emaila",
-  "admin.config.email.enable-share-email-recipients.description": "Želite li omogućiti dijeljenje putem emaila. Omogućite ovo samo ako je SMTP aktiviran.",
+  "admin.config.email.enable-share-email-recipients.description": "Whether to allow email sharing with recipients. This can only be enabled if SMTP is activated.",
   "admin.config.email.share-recipients-subject": "Naslov emaila za primatelje dijeljenja",
   "admin.config.email.share-recipients-subject.description": "Predmet emaila koji se šalje primateljima dijeljenja.",
   "admin.config.email.share-recipients-message": "Poruka za primatelje dijeljenja",
