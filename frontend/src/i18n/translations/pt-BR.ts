01cc27c7944afbb5c5ef1847dbbe15ed844c959c
smp46 <me@smp46.me>
2026-06-12 09:07:56 +0200

    New translations en-us.ts (Portuguese, Brazilian)
    [ci skip]

--- frontend/src/i18n/translations/pt-BR.ts
+++ frontend/src/i18n/translations/pt-BR.ts
@@ -429,7 +429,7 @@ export default {
   "admin.config.share.allow-admin-access-all-shares": "Allow admin access to all shares",
   "admin.config.share.allow-admin-access-all-shares.description": "Allow administrators to access all shares, even if they are password protected, expired or deleted.",
   "admin.config.share.file-retention-period": "File retention period",
-  "admin.config.share.file-retention-period.description": "How long files are kept after a share expires or gets deleted. Only useful if the 'Allow admin access to all shares' is also enabled.",
+  "admin.config.share.file-retention-period.description": "How long files are kept after a share expires or gets deleted. Only useful if the 'Allow admin access to all shares' is also enabled. Set to -1 to keep files forever.",
   "admin.config.smtp.enabled": "Habilitar",
   "admin.config.smtp.enabled.description": "Se o SMTP está habilitado. Apenas defina como verdadeiro se você digitou o servidor, porta, e-mail, usuário e senha do seu servidor SMTP.",
   "admin.config.smtp.host": "Servidor",
