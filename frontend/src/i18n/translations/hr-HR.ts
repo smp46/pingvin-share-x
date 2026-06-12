3d6ef0a22e91fcaf3c5ae1bf868dd4ff5dbb464a
smp46 <me@smp46.me>
2026-06-12 09:07:59 +0200

    New translations en-us.ts (Croatian)
    [ci skip]

--- frontend/src/i18n/translations/hr-HR.ts
+++ frontend/src/i18n/translations/hr-HR.ts
@@ -429,7 +429,7 @@ export default {
   "admin.config.share.allow-admin-access-all-shares": "Allow admin access to all shares",
   "admin.config.share.allow-admin-access-all-shares.description": "Allow administrators to access all shares, even if they are password protected, expired or deleted.",
   "admin.config.share.file-retention-period": "File retention period",
-  "admin.config.share.file-retention-period.description": "How long files are kept after a share expires or gets deleted. Only useful if the 'Allow admin access to all shares' is also enabled.",
+  "admin.config.share.file-retention-period.description": "How long files are kept after a share expires or gets deleted. Only useful if the 'Allow admin access to all shares' is also enabled. Set to -1 to keep files forever.",
   "admin.config.smtp.enabled": "Omogući",
   "admin.config.smtp.enabled.description": "Je li SMTP omogućen. Postavite na 'točno' samo ako ste unijeli host, port, email, korisničko ime i lozinku vašeg SMTP servera.",
   "admin.config.smtp.host": "Domaćin",
