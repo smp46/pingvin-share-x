06d1141fd87cc9583b910bad68bb9ddc4c99d4ff
smp46 <me@smp46.me>
2026-06-12 09:08:00 +0200

    New translations en-us.ts (Estonian)
    [ci skip]

--- frontend/src/i18n/translations/et-EE.ts
+++ frontend/src/i18n/translations/et-EE.ts
@@ -429,7 +429,7 @@ export default {
   "admin.config.share.allow-admin-access-all-shares": "Allow admin access to all shares",
   "admin.config.share.allow-admin-access-all-shares.description": "Allow administrators to access all shares, even if they are password protected, expired or deleted.",
   "admin.config.share.file-retention-period": "File retention period",
-  "admin.config.share.file-retention-period.description": "How long files are kept after a share expires or gets deleted. Only useful if the 'Allow admin access to all shares' is also enabled.",
+  "admin.config.share.file-retention-period.description": "How long files are kept after a share expires or gets deleted. Only useful if the 'Allow admin access to all shares' is also enabled. Set to -1 to keep files forever.",
   "admin.config.smtp.enabled": "Luba",
   "admin.config.smtp.enabled.description": "Kas SMTP on lubatud. Sea see tõeks ainult siis, kui oled sisestanud oma SMTP serveri hosti, pordi, e-posti, kasutajanime ja parooli.",
   "admin.config.smtp.host": "Host",
