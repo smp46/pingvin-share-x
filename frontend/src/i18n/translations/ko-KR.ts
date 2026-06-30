ffb17f8297be43a2aacf351ab806df2c4639f17c
smp46 <me@smp46.me>
2026-06-30 14:50:31 +0300

    New translations en-us.ts (Korean)
    [ci skip]

--- frontend/src/i18n/translations/ko-KR.ts
+++ frontend/src/i18n/translations/ko-KR.ts
@@ -451,6 +451,8 @@ export default {
   "admin.config.share.auto-open-share-modal": "공유 생성 창 자동 열기",
   "admin.config.share.auto-open-share-modal.description": "사용자가 파일을 선택하면 공유 생성 창이 자동으로 나타나서 버튼을 수동으로 클릭할 필요가 없습니다.",
   "admin.config.share.allow-admin-access-all-shares": "Allow admin access to all shares",
+  "admin.config.share.reverse-share-simple-only": "Force reverse share simple mode",
+  "admin.config.share.reverse-share-simple-only.description": "Force reverse shares to be created in simple mode. If disabled, the creator of the reverse share can choose between simple and advanced mode.",
   "admin.config.share.allow-admin-access-all-shares.description": "Allow administrators to access all shares, even if they are password protected, expired or deleted.",
   "admin.config.share.file-retention-period": "File retention period",
   "admin.config.share.file-retention-period.description": "How long files are kept after a share expires or gets deleted. Only useful if the 'Allow admin access to all shares' is also enabled. Set to -1 to keep files forever.",
