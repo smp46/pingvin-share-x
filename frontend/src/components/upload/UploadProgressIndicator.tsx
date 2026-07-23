f9bc5840b93fe2d3f491966149bb83ec8e32eaac
smp46 <me@smp46.me>
2026-07-23 21:03:20 +1000

    feat(uploads): text based progress indicator

--- frontend/src/components/upload/UploadProgressIndicator.tsx
+++ frontend/src/components/upload/UploadProgressIndicator.tsx
@@ -1,4 +1,4 @@
-import { Loader, RingProgress } from "@mantine/core";
+import { Loader, RingProgress, Text } from "@mantine/core";
 import { TbCircleCheck } from "react-icons/tb";
 const UploadProgressIndicator = ({ progress }: { progress: number }) => {
   if (progress > 0 && progress < 100) {
@@ -6,7 +6,12 @@ const UploadProgressIndicator = ({ progress }: { progress: number }) => {
       <RingProgress
         sections={[{ value: progress, color: "victoria" }]}
         thickness={3}
-        size={25}
+        size={40}
+        label={
+          <Text size="xs" color="victoria" weight={700} align="center">
+            {Math.round(progress)}%
+          </Text>
+        }
       />
     );
   } else if (progress >= 100) {
