bf4036a925f7d6e5b9d4a9260631d52d021a09bd
smp46 <me@smp46.me>
2026-08-03 03:00:09 +1000

    refactor: move url customisation to components

---
+++ frontend/src/components/share/CustomUrlInput.tsx
@@ -0,0 +1,66 @@
+import { Button, Group, Text, TextInput } from "@mantine/core";
+import { UseFormReturnType } from "@mantine/form";
+import { FormattedMessage } from "react-intl";
+import useTranslate from "../../hooks/useTranslate.hook";
+import { generateShareId } from "../../utils/share.util";
+
+interface CustomUrlInputProps {
+  form: UseFormReturnType<any>;
+  fieldName?: string;
+  shareIdLength: number;
+  appUrl: string;
+  defaultAppUrl: string;
+  pathPrefix?: string;
+}
+
+const CustomUrlInput = ({
+  form,
+  fieldName = "link",
+  shareIdLength,
+  appUrl,
+  defaultAppUrl,
+  pathPrefix = "/s/",
+}: CustomUrlInputProps) => {
+  const t = useTranslate();
+  const fieldValue = form.values[fieldName] || "";
+  const hasError = !!form.errors[fieldName];
+
+  const baseUrl =
+    appUrl !== defaultAppUrl ? appUrl : typeof window !== "undefined" ? window.location.origin : "";
+
+  return (
+    <>
+      <Group align={hasError ? "center" : "flex-end"}>
+        <TextInput
+          style={{ flex: "1" }}
+          variant="filled"
+          label={t("upload.modal.link.label")}
+          placeholder="myAwesomeShare"
+          {...form.getInputProps(fieldName)}
+        />
+        <Button
+          style={{ flex: "0 0 auto" }}
+          variant="outline"
+          onClick={() =>
+            form.setFieldValue(fieldName, generateShareId(shareIdLength))
+          }
+        >
+          <FormattedMessage id="common.button.generate" />
+        </Button>
+      </Group>
+
+      <Text
+        truncate
+        italic
+        size="xs"
+        sx={(theme) => ({
+          color: theme.colors.gray[6],
+        })}
+      >
+        {`${baseUrl}${pathPrefix}${fieldValue}`}
+      </Text>
+    </>
+  );
+};
+
+export default CustomUrlInput;
