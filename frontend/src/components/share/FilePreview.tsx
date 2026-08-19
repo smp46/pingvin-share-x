44e09d536b1b31e36223fcb5ad34b0b8421a1c6b
smp46 <me@smp46.me>
2026-08-20 03:08:13 +1000

    fix(filePreview): use html <a> instead of Next Link for file link

--- frontend/src/components/share/FilePreview.tsx
+++ frontend/src/components/share/FilePreview.tsx
@@ -6,9 +6,7 @@ import {
   Title,
   useMantineTheme,
 } from "@mantine/core";
-import { modals } from "@mantine/modals";
 import Markdown, { MarkdownToJSX } from "markdown-to-jsx";
-import Link from "next/link";
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { FormattedMessage } from "react-intl";
 import api from "../../services/api.service";
@@ -46,9 +44,9 @@ const FilePreview = ({
       </FilePreviewContext.Provider>
       <Button
         variant="subtle"
-        component={Link}
-        onClick={() => modals.closeAll()}
+        component="a"
         target="_blank"
+        rel="noreferrer"
         href={`/api/shares/${shareId}/files/${fileId}?download=false`}
       >
         View original file
