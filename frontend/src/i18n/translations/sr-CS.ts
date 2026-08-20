36146aa7b619c23cd04798b134f21145a33c0a86
smp46 <me@smp46.me>
2026-08-20 17:41:12 +0200

    New translations en-us.ts (Serbian (Latin))
    [ci skip]

--- frontend/src/i18n/translations/sr-CS.ts
+++ frontend/src/i18n/translations/sr-CS.ts
@@ -355,6 +355,7 @@ export default {
   "share.modal.file-link": "Veza datoteke",
   "share.table.name": "Naziv",
   "share.table.size": "Veličina",
+  "share.modal.file-preview.original_file": "View original file",
   "share.modal.file-preview.error.not-supported.title": "Pregled nije podržan",
   "share.modal.file-preview.error.not-supported.description": "Pregledi nisu podržani za ovaj tip datoteka. Molimo preuzmite fajl da biste ga pregledali.",
   // END /share/[id]
