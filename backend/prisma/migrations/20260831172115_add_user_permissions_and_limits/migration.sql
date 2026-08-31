e2253c3aeb89b172c9ef0ded385a6b1a5f6e5079
smp46 <me@smp46.me>
2026-09-01 03:23:24 +1000

    feat: schema for new user perms

---
+++ backend/prisma/migrations/20260831172115_add_user_permissions_and_limits/migration.sql
@@ -0,0 +1,34 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "ldapDN" TEXT,
+    "shareSizeLimit" TEXT,
+    "storageQuotaLimit" TEXT,
+    "allowShare" BOOLEAN NOT NULL DEFAULT true,
+    "allowCreateReverseShares" BOOLEAN NOT NULL DEFAULT true,
+    "maxShares" INTEGER,
+    "maxReverseShares" INTEGER,
+    "totpEnabled" BOOLEAN NOT NULL DEFAULT false,
+    "totpVerified" BOOLEAN NOT NULL DEFAULT false,
+    "totpSecret" TEXT,
+    "isActivated" BOOLEAN NOT NULL DEFAULT true,
+    "activationToken" TEXT,
+    "activationTokenExpiresAt" DATETIME
+);
+INSERT INTO "new_User" ("activationToken", "activationTokenExpiresAt", "createdAt", "email", "id", "isActivated", "isAdmin", "ldapDN", "password", "shareSizeLimit", "storageQuotaLimit", "totpEnabled", "totpSecret", "totpVerified", "updatedAt", "username") SELECT "activationToken", "activationTokenExpiresAt", "createdAt", "email", "id", "isActivated", "isAdmin", "ldapDN", "password", "shareSizeLimit", "storageQuotaLimit", "totpEnabled", "totpSecret", "totpVerified", "updatedAt", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_ldapDN_key" ON "User"("ldapDN");
+CREATE UNIQUE INDEX "User_activationToken_key" ON "User"("activationToken");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
