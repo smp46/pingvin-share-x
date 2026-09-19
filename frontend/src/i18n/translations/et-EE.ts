14f4bc827ec75c95e75ae41c64c5a7a0aedc97a5
smp46 <me@smp46.me>
2026-09-19 18:19:58 +0200

    New translations en-us.ts (Estonian)
    [ci skip]

--- frontend/src/i18n/translations/et-EE.ts
+++ frontend/src/i18n/translations/et-EE.ts
@@ -287,7 +287,10 @@ export default {
   "upload.error.not-allowed.description": "Your account does not have permission to upload or create shares.",
   // Dropzone.tsx
   "upload.dropzone.title": "Laadi üles failid",
-  "upload.dropzone.description": "Lohista failid või kaustad siia jagamise alustamiseks või kasuta lõikelaualt tekstisisu üleslaadimiseks klahvikombinatsiooni 'Ctrl+V'. Me aktsepteerime faile kogumahus kuni {maxSize}.",
+  "upload.dropzone.description.mobile": "Tap here to select individual files, photos, or videos. To upload a folder, use the button below. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.mobile.no-folder": "Tap here to select individual files, photos, or videos. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop": "Click here or drag and drop individual files, photos, or videos. Use the button below to upload a folder, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop.no-folder": "Click here or drag and drop individual files, photos, or videos, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
   "upload.dropzone.notify.file-too-big": "Sinu failid ületavad lubatud suuruse {maxSize}.",
   "upload.button.folder": "Laadi üles kaust",
   "upload.button.folder.append": "Lisa kaust",
