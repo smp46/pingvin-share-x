e2d295858455beeb703e3e9429c9a51137633cdb
smp46 <me@smp46.me>
2026-09-19 18:19:31 +0200

    New translations en-us.ts (Finnish)
    [ci skip]

--- frontend/src/i18n/translations/fi-FI.ts
+++ frontend/src/i18n/translations/fi-FI.ts
@@ -287,7 +287,10 @@ export default {
   "upload.error.not-allowed.description": "Your account does not have permission to upload or create shares.",
   // Dropzone.tsx
   "upload.dropzone.title": "Pilvetä tiedostoja",
-  "upload.dropzone.description": "Vedä ja pudota tiedostoja tai kansioita tähän aloittaaksesi jakamisen tai paina 'Ctrl+V' ladataksesi tekstisisältöä leikepöydältä. Hyväksymme tiedostoja yhteensä enintään {maxSize} asti.",
+  "upload.dropzone.description.mobile": "Tap here to select individual files, photos, or videos. To upload a folder, use the button below. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.mobile.no-folder": "Tap here to select individual files, photos, or videos. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop": "Click here or drag and drop individual files, photos, or videos. Use the button below to upload a folder, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop.no-folder": "Click here or drag and drop individual files, photos, or videos, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
   "upload.dropzone.notify.file-too-big": "Tiedostojen enimmäiskoko ylittää {maxSize} -arvon enimmäismäärän.",
   "upload.button.folder": "Lataa kansio",
   "upload.button.folder.append": "Lisää kansio",
