6ee8677ac78f6e8f4c402a5a83dd273f2498f10d
smp46 <me@smp46.me>
2026-09-19 18:19:35 +0200

    New translations en-us.ts (Japanese)
    [ci skip]

--- frontend/src/i18n/translations/ja-JP.ts
+++ frontend/src/i18n/translations/ja-JP.ts
@@ -287,7 +287,10 @@ export default {
   "upload.error.not-allowed.description": "Your account does not have permission to upload or create shares.",
   // Dropzone.tsx
   "upload.dropzone.title": "ファイルをアップロード",
-  "upload.dropzone.description": "ファイルやフォルダをここにドラッグ＆ドロップして共有を開始するか、'Ctrl+V' でクリップボードからテキストコンテンツをアップロードします。合計で最大 {maxSize} までのファイルを受け付けます。",
+  "upload.dropzone.description.mobile": "Tap here to select individual files, photos, or videos. To upload a folder, use the button below. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.mobile.no-folder": "Tap here to select individual files, photos, or videos. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop": "Click here or drag and drop individual files, photos, or videos. Use the button below to upload a folder, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop.no-folder": "Click here or drag and drop individual files, photos, or videos, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
   "upload.dropzone.notify.file-too-big": "アップロードしようとしたファイルは、最大ファイルサイズの{maxSize} を超えています。",
   "upload.button.folder": "フォルダをアップロード",
   "upload.button.folder.append": "フォルダを追加",
