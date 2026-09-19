3e79220b87d6920d54c1259032e7a3fc86825193
smp46 <me@smp46.me>
2026-09-19 18:19:28 +0200

    New translations en-us.ts (German)
    [ci skip]

--- frontend/src/i18n/translations/de-DE.ts
+++ frontend/src/i18n/translations/de-DE.ts
@@ -287,7 +287,10 @@ export default {
   "upload.error.not-allowed.description": "Your account does not have permission to upload or create shares.",
   // Dropzone.tsx
   "upload.dropzone.title": "Dateien hochladen",
-  "upload.dropzone.description": "Ziehe Dateien oder Ordner hierher, um deine Freigabe zu starten, oder drücke 'Strg+V', um Textinhalte aus der Zwischenablage hochzuladen. Wir akzeptieren nur Dateien bis insgesamt {maxSize}.",
+  "upload.dropzone.description.mobile": "Tap here to select individual files, photos, or videos. To upload a folder, use the button below. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.mobile.no-folder": "Tap here to select individual files, photos, or videos. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop": "Click here or drag and drop individual files, photos, or videos. Use the button below to upload a folder, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop.no-folder": "Click here or drag and drop individual files, photos, or videos, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
   "upload.dropzone.notify.file-too-big": "Ihre Dateien überschreiten die maximale Freigabegröße von {maxSize}.",
   "upload.button.folder": "Ordner hochladen",
   "upload.button.folder.append": "Ordner anhängen",
