5b22699f2505d29ad7e4ee46fd86d346fe5f8bd8
smp46 <me@smp46.me>
2026-09-19 18:19:53 +0200

    New translations en-us.ts (Portuguese, Brazilian)
    [ci skip]

--- frontend/src/i18n/translations/pt-BR.ts
+++ frontend/src/i18n/translations/pt-BR.ts
@@ -287,7 +287,10 @@ export default {
   "upload.error.not-allowed.description": "Your account does not have permission to upload or create shares.",
   // Dropzone.tsx
   "upload.dropzone.title": "Carregar arquivos",
-  "upload.dropzone.description": "Arraste e solte arquivos ou pastas aqui para iniciar seu compartilhamento ou 'Ctrl+V' para carregar conteúdo de texto da área de transferência. Aceitamos apenas arquivos até {maxSize} no total.",
+  "upload.dropzone.description.mobile": "Tap here to select individual files, photos, or videos. To upload a folder, use the button below. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.mobile.no-folder": "Tap here to select individual files, photos, or videos. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop": "Click here or drag and drop individual files, photos, or videos. Use the button below to upload a folder, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop.no-folder": "Click here or drag and drop individual files, photos, or videos, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
   "upload.dropzone.notify.file-too-big": "Seus arquivos excedem o tamanho máximo do compartilhamento {maxSize}.",
   "upload.button.folder": "Carregar pasta",
   "upload.button.folder.append": "Anexar pasta",
