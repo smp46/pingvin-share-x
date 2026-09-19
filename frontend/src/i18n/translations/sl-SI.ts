c6da7d7810f09a92716d3bd84849b608d5b08392
smp46 <me@smp46.me>
2026-09-19 18:19:42 +0200

    New translations en-us.ts (Slovenian)
    [ci skip]

--- frontend/src/i18n/translations/sl-SI.ts
+++ frontend/src/i18n/translations/sl-SI.ts
@@ -287,7 +287,10 @@ export default {
   "upload.error.not-allowed.description": "Your account does not have permission to upload or create shares.",
   // Dropzone.tsx
   "upload.dropzone.title": "Naloži datoteke",
-  "upload.dropzone.description": "Povlecite in spustite datoteke ali mape sem, da začnete deljenje, ali pritisnite 'Ctrl+V' za nalaganje besedilne vsebine iz odložišča. Sprejemamo le datoteke do skupno {maxSize}.",
+  "upload.dropzone.description.mobile": "Tap here to select individual files, photos, or videos. To upload a folder, use the button below. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.mobile.no-folder": "Tap here to select individual files, photos, or videos. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop": "Click here or drag and drop individual files, photos, or videos. Use the button below to upload a folder, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop.no-folder": "Click here or drag and drop individual files, photos, or videos, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
   "upload.dropzone.notify.file-too-big": "Vaše datoteke presegajo največjo dovoljeno velikost {maxSize}.",
   "upload.button.folder": "Naloži mapo",
   "upload.button.folder.append": "Dodaj mapo",
