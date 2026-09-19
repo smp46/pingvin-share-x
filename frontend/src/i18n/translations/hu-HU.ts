d8257075833ba93125b88981ffddcf1509b11b7b
smp46 <me@smp46.me>
2026-09-19 18:19:32 +0200

    New translations en-us.ts (Hungarian)
    [ci skip]

--- frontend/src/i18n/translations/hu-HU.ts
+++ frontend/src/i18n/translations/hu-HU.ts
@@ -287,7 +287,10 @@ export default {
   "upload.error.not-allowed.description": "Your account does not have permission to upload or create shares.",
   // Dropzone.tsx
   "upload.dropzone.title": "Fájlok feltöltése",
-  "upload.dropzone.description": "Húzza ide a fájlokat vagy mappákat a megosztás megkezdéséhez, vagy használja a 'Ctrl+V' billentyűkombinációt a vágólapon lévő szöveges tartalom feltöltéséhez. Összesen legfeljebb {maxSize} méretű fájlokat fogadunk el.",
+  "upload.dropzone.description.mobile": "Tap here to select individual files, photos, or videos. To upload a folder, use the button below. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.mobile.no-folder": "Tap here to select individual files, photos, or videos. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop": "Click here or drag and drop individual files, photos, or videos. Use the button below to upload a folder, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
+  "upload.dropzone.description.desktop.no-folder": "Click here or drag and drop individual files, photos, or videos, or press {shortcut} to upload text from your clipboard. Maximum total upload size: {maxSize}.",
   "upload.dropzone.notify.file-too-big": "A fájlok mérete meghaladja a megengedett {maxSize} méretet.",
   "upload.button.folder": "Mappa feltöltése",
   "upload.button.folder.append": "Mappa hozzáfűzése",
