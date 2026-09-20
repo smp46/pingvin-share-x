b4985b29e30cc061aaa9f898ef4f9b6b2142eb53
smp46 <me@smp46.me>
2026-09-20 08:08:42 +0200

    New translations en-us.ts (Finnish)
    [ci skip]

--- frontend/src/i18n/translations/fi-FI.ts
+++ frontend/src/i18n/translations/fi-FI.ts
@@ -405,6 +405,8 @@ export default {
   "admin.config.category.oauth": "Sosiaalisen median kirjautuminen",
   "admin.config.general.app-name": "Sovelluksen nimi",
   "admin.config.general.app-name.description": "Sovelluksen nimi",
+  "admin.config.general.show-auth-buttons": "Show auth buttons",
+  "admin.config.general.show-auth-buttons.description": "Whether to show sign-in and sign-up buttons on share and upload pages.",
   "admin.config.general.default-language": "Oletuskieli",
   "admin.config.general.default-language.description": "This applies to all users. Each user can still choose a language in their profile.",
   "admin.config.appearance.theme-primary-color": "Teeman pääväri",
