961c7db410306db47310ac499feb039832da0b50
smp46 <me@smp46.me>
2026-09-20 08:09:00 +0200

    New translations en-us.ts (Chinese Traditional)
    [ci skip]

--- frontend/src/i18n/translations/zh-TW.ts
+++ frontend/src/i18n/translations/zh-TW.ts
@@ -405,6 +405,8 @@ export default {
   "admin.config.category.oauth": "第三方登入",
   "admin.config.general.app-name": "App 名稱",
   "admin.config.general.app-name.description": "這個 App 的名稱",
+  "admin.config.general.show-auth-buttons": "Show auth buttons",
+  "admin.config.general.show-auth-buttons.description": "Whether to show sign-in and sign-up buttons on share and upload pages.",
   "admin.config.general.default-language": "預設語言",
   "admin.config.general.default-language.description": "This applies to all users. Each user can still choose a language in their profile.",
   "admin.config.appearance.theme-primary-color": "主題主色",
