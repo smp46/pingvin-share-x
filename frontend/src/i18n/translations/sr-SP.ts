ea6b23a462a3f8241dbbbc4f93dfb4df41aa2250
smp46 <me@smp46.me>
2026-09-20 08:08:53 +0200

    New translations en-us.ts (Serbian (Cyrillic))
    [ci skip]

--- frontend/src/i18n/translations/sr-SP.ts
+++ frontend/src/i18n/translations/sr-SP.ts
@@ -405,6 +405,8 @@ export default {
   "admin.config.category.oauth": "Друштвена пријава",
   "admin.config.general.app-name": "Назив апликације",
   "admin.config.general.app-name.description": "Назив апликације",
+  "admin.config.general.show-auth-buttons": "Show auth buttons",
+  "admin.config.general.show-auth-buttons.description": "Whether to show sign-in and sign-up buttons on share and upload pages.",
   "admin.config.general.default-language": "Подразумевани језик",
   "admin.config.general.default-language.description": "This applies to all users. Each user can still choose a language in their profile.",
   "admin.config.appearance.theme-primary-color": "Примарна боја теме",
