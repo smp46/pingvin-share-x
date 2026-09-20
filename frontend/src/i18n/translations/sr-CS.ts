70ec3e216954cc96c5f570ef3d3f65ed1a89a9b4
smp46 <me@smp46.me>
2026-09-20 08:09:10 +0200

    New translations en-us.ts (Serbian (Latin))
    [ci skip]

--- frontend/src/i18n/translations/sr-CS.ts
+++ frontend/src/i18n/translations/sr-CS.ts
@@ -405,6 +405,8 @@ export default {
   "admin.config.category.oauth": "Social login",
   "admin.config.general.app-name": "Naziv aplikacije",
   "admin.config.general.app-name.description": "Naziv aplikacije",
+  "admin.config.general.show-auth-buttons": "Show auth buttons",
+  "admin.config.general.show-auth-buttons.description": "Whether to show sign-in and sign-up buttons on share and upload pages.",
   "admin.config.general.default-language": "Podrazumevani jezik",
   "admin.config.general.default-language.description": "This applies to all users. Each user can still choose a language in their profile.",
   "admin.config.appearance.theme-primary-color": "Primarna boja teme",
