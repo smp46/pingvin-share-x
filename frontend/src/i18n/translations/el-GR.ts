a09991b4557e49da7396ea7ae4ba53a708f66982
smp46 <me@smp46.me>
2026-09-20 08:08:41 +0200

    New translations en-us.ts (Greek)
    [ci skip]

--- frontend/src/i18n/translations/el-GR.ts
+++ frontend/src/i18n/translations/el-GR.ts
@@ -405,6 +405,8 @@ export default {
   "admin.config.category.oauth": "Σύνδεση με λογαριασμό μέσων κοινωνικού δικτύου",
   "admin.config.general.app-name": "Όνομα εφαρμογής",
   "admin.config.general.app-name.description": "Ονομασία της εφαρμογής",
+  "admin.config.general.show-auth-buttons": "Show auth buttons",
+  "admin.config.general.show-auth-buttons.description": "Whether to show sign-in and sign-up buttons on share and upload pages.",
   "admin.config.general.default-language": "Προεπιλεγμένη γλώσσα",
   "admin.config.general.default-language.description": "This applies to all users. Each user can still choose a language in their profile.",
   "admin.config.appearance.theme-primary-color": "Κύριο χρώμα θέματος",
