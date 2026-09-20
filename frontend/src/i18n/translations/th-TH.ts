fc2a8e04a987b370caaad9ea7bb99a7811f9e69a
smp46 <me@smp46.me>
2026-09-20 08:09:06 +0200

    New translations en-us.ts (Thai)
    [ci skip]

--- frontend/src/i18n/translations/th-TH.ts
+++ frontend/src/i18n/translations/th-TH.ts
@@ -405,6 +405,8 @@ export default {
   "admin.config.category.oauth": "เข้าสู่ระบบด้วยบัญชีโซเชียล",
   "admin.config.general.app-name": "ชื่อแอพ",
   "admin.config.general.app-name.description": "ชื่อแอพพลิเคชัน",
+  "admin.config.general.show-auth-buttons": "Show auth buttons",
+  "admin.config.general.show-auth-buttons.description": "Whether to show sign-in and sign-up buttons on share and upload pages.",
   "admin.config.general.default-language": "ภาษาเริ่มต้น",
   "admin.config.general.default-language.description": "This applies to all users. Each user can still choose a language in their profile.",
   "admin.config.appearance.theme-primary-color": "สีหลักของธีม",
