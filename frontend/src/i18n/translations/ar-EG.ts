75718f5b91642cbc7839ff6b9dfefda41314ffce
smp46 <me@smp46.me>
2026-09-20 08:09:13 +0200

    New translations en-us.ts (Arabic, Egypt)
    [ci skip]

--- frontend/src/i18n/translations/ar-EG.ts
+++ frontend/src/i18n/translations/ar-EG.ts
@@ -405,6 +405,8 @@ export default {
   "admin.config.category.oauth": "الدخول بحساب تواصل اجتماعي",
   "admin.config.general.app-name": "اسم التطبيق",
   "admin.config.general.app-name.description": "اسم التطبيق",
+  "admin.config.general.show-auth-buttons": "Show auth buttons",
+  "admin.config.general.show-auth-buttons.description": "Whether to show sign-in and sign-up buttons on share and upload pages.",
   "admin.config.general.default-language": "اللغة الافتراضية",
   "admin.config.general.default-language.description": "This applies to all users. Each user can still choose a language in their profile.",
   "admin.config.appearance.theme-primary-color": "اللون الأساسي للمظهر",
