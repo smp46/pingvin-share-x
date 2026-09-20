bef6756f903db643e2aea7c753a5ba271fc55578
smp46 <me@smp46.me>
2026-09-20 08:08:58 +0200

    New translations en-us.ts (Ukrainian)
    [ci skip]

--- frontend/src/i18n/translations/uk-UA.ts
+++ frontend/src/i18n/translations/uk-UA.ts
@@ -405,6 +405,8 @@ export default {
   "admin.config.category.oauth": "Вхід через соцмережі",
   "admin.config.general.app-name": "Назва програми",
   "admin.config.general.app-name.description": "Видима назва додатка",
+  "admin.config.general.show-auth-buttons": "Show auth buttons",
+  "admin.config.general.show-auth-buttons.description": "Whether to show sign-in and sign-up buttons on share and upload pages.",
   "admin.config.general.default-language": "Мова за замовчуванням",
   "admin.config.general.default-language.description": "This applies to all users. Each user can still choose a language in their profile.",
   "admin.config.appearance.theme-primary-color": "Основний колір теми",
