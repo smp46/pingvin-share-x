f37d6c19ac31fe8f6231a75b849e160725180d30
smp46 <me@smp46.me>
2026-09-20 08:08:55 +0200

    New translations en-us.ts (Swedish)
    [ci skip]

--- frontend/src/i18n/translations/sv-SE.ts
+++ frontend/src/i18n/translations/sv-SE.ts
@@ -405,6 +405,8 @@ export default {
   "admin.config.category.oauth": "Inloggning via sociala nätverk",
   "admin.config.general.app-name": "Appnamn",
   "admin.config.general.app-name.description": "Namn på applikationen",
+  "admin.config.general.show-auth-buttons": "Show auth buttons",
+  "admin.config.general.show-auth-buttons.description": "Whether to show sign-in and sign-up buttons on share and upload pages.",
   "admin.config.general.default-language": "Standardspråk",
   "admin.config.general.default-language.description": "This applies to all users. Each user can still choose a language in their profile.",
   "admin.config.appearance.theme-primary-color": "Temats primärfärg",
