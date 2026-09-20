ff6f3c7e62ffc0c3734747eda345ebf44860b3dd
smp46 <me@smp46.me>
2026-09-20 08:08:37 +0200

    New translations en-us.ts (Czech)
    [ci skip]

--- frontend/src/i18n/translations/cs-CZ.ts
+++ frontend/src/i18n/translations/cs-CZ.ts
@@ -405,6 +405,8 @@ export default {
   "admin.config.category.oauth": "Přihlášení přes sociální sítě",
   "admin.config.general.app-name": "Název aplikace",
   "admin.config.general.app-name.description": "Název aplikace",
+  "admin.config.general.show-auth-buttons": "Show auth buttons",
+  "admin.config.general.show-auth-buttons.description": "Whether to show sign-in and sign-up buttons on share and upload pages.",
   "admin.config.general.default-language": "Výchozí jazyk",
   "admin.config.general.default-language.description": "This applies to all users. Each user can still choose a language in their profile.",
   "admin.config.appearance.theme-primary-color": "Primární barva motivu",
