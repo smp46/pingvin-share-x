b468136c33f5dd7882f2c35511fea43af4ef4e4a
smp46 <me@smp46.me>
2026-09-20 08:08:45 +0200

    New translations en-us.ts (Italian)
    [ci skip]

--- frontend/src/i18n/translations/it-IT.ts
+++ frontend/src/i18n/translations/it-IT.ts
@@ -405,6 +405,8 @@ export default {
   "admin.config.category.oauth": "Login tramite Social",
   "admin.config.general.app-name": "Nome dell'App",
   "admin.config.general.app-name.description": "Nome dell' applicazione",
+  "admin.config.general.show-auth-buttons": "Show auth buttons",
+  "admin.config.general.show-auth-buttons.description": "Whether to show sign-in and sign-up buttons on share and upload pages.",
   "admin.config.general.default-language": "Lingua predefinita",
   "admin.config.general.default-language.description": "This applies to all users. Each user can still choose a language in their profile.",
   "admin.config.appearance.theme-primary-color": "Colore primario del tema",
